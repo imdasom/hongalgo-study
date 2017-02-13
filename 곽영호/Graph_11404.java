565fc025ef3216381f81747861631172427716f1
YoungHo Kwak <jason.uphiller@gmail.com>
2017-02-13 13:36:52 +0900

    Update Graph_11404.java
    주석 추가

--- 곽영호/Graph_11404.java
+++ 곽영호/Graph_11404.java
@@ -28,7 +28,11 @@ public class Graph_11404 {
             }
         }
         scan.close();
-
+        
+        // map[i][j] = min(map[i][k] + map[k][j]) 라고 생각
+	    // map[i][j]를 i에서 j까지 가는 최소경로라고 정의할때
+	    // 모든 노드 포함된 맵이 반영되어야 최소경로를 알 수 있다.
+	    // 즉, 노드를 하나씩 추가하여 모두 추가했을때, 최소경로를 알 수 있다.
         for (int k = 1; k <= cityCnt; k++) {
             for (int i = 1; i <= cityCnt; i++) {
                 for (int j = 1; j <= cityCnt; j++) {
